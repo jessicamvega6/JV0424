553abae38172eeee8f438a2b77d1f562713294bf
jessicamvega6 <jessicamvega6@gmail.com>
2024-04-20 16:33:44 -0500

    Domain classes init commit
    Adding the rental agreement object along with Tool object. TBD if Tool will become an enum

---
+++ src/main/java/com/code/interviewDemo/domain/RentalAgreement.java
@@ -0,0 +1,21 @@
+package com.code.interviewDemo.domain;
+
+import java.util.Date;
+
+public class RentalAgreement {
+
+    private String toolCode;
+    private String toolType;
+    private String toolBrand;
+    private int rentalDays;
+    private Date checkoutDate;
+    private Date dueDate;
+    private Long dailyRentalCharge;
+    private Integer chargeDays;
+    private Long preDiscountCharge;
+    private Long discountCharge;
+    private int discountPercent;
+    private Long discountAmount;
+    private Long finalChagre;
+
+}
---
+++ src/main/java/com/code/interviewDemo/domain/Tool.java
@@ -0,0 +1,10 @@
+package com.code.interviewDemo.domain;
+
+public class Tool {
+
+    private String toolCode;
+
+    private String toolType;
+
+    private String brand;
+}
