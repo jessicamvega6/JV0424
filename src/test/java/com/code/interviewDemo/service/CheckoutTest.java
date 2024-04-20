e28ffdc13f2748c731d5f1d003f916d26df580fb
jessicamvega6 <jessicamvega6@gmail.com>
2024-04-20 17:19:51 -0500

    Refactored Tests to expect an Exception and use org.junit.Test
    Replaced junit jupitor with library I'm more familiar with

--- src/test/java/com/code/interviewDemo/service/CheckoutTest.java
+++ src/test/java/com/code/interviewDemo/service/CheckoutTest.java
@@ -1,54 +1,51 @@
 package com.code.interviewDemo.service;
 
 import com.code.interviewDemo.domain.RentalAgreement;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.*;
 
-class CheckoutTest {
+public class CheckoutTest {
 
     Checkout checkout = new Checkout();
 
-    @BeforeEach
-    void setUp() {
+    @Test(expected = RuntimeException.class)
+    public void checkout_discountOver100_fails() throws Exception {
+        checkout.checkout("JAKR", "9/3/15", 5, 101);
     }
 
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void checkout_discountOver100_fails() {
-        RentalAgreement agreement = checkout.checkout("JAKR", "9/3/15", 5, 101);
-        assertThat(agreement, equalTo(null));
-    }
-
-    @Test
-    void checkout_discountNegative_fails() {
+    @Test(expected = RuntimeException.class)
+    public void checkout_discountNegative_fails() throws Exception {
         RentalAgreement agreement = checkout.checkout("JAKR", "9/3/15", 5, -11);
-        assertThat(agreement, equalTo(null));
     }
 
-    @Test
-    void checkout_negativeNumberOfDaysRented_fails() {
+    @Test(expected = RuntimeException.class)
+    public void checkout_negativeNumberOfDaysRented_fails() throws Exception {
         RentalAgreement agreement = checkout.checkout("JAKR", "9/13/14", -2, 0);
-        assertThat(agreement, equalTo(null));
     }
 
-    @Test
-    void checkout_toolDoesntExist_fails() {
+    @Test(expected = RuntimeException.class)
+    public void checkout_toolDoesntExist_fails() throws Exception {
         RentalAgreement agreement = checkout.checkout("JR", "9/13/14", 2, 0);
-        assertThat(agreement, equalTo(null));
     }
 
-    @Test
-    void checkout_checkoutDateInFuture_fails() {
+    @Test(expected = RuntimeException.class)
+    public void checkout_checkoutDateInFuture_fails() throws Exception {
         RentalAgreement agreement = checkout.checkout("JR", "9/13/14", 2, 0);
-        assertThat(agreement, equalTo(null));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void checkout_dateInFormatyyyymdd_fails() throws Exception {
+        RentalAgreement agreement = checkout.checkout("JR", "2014/9/13", 2, 0);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void checkout_dateInFormatyyyymmdd_fails() throws Exception {
+        RentalAgreement agreement = checkout.checkout("JR", "2014/09/13", 2, 0);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void checkout_dateInFormatWithDashes_fails() throws Exception {
+        RentalAgreement agreement = checkout.checkout("JR", "9-13-2014", 2, 0);
     }
 
 }
