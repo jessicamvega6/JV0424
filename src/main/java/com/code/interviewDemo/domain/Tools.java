da6e8f30d42a5e9853ebf9f7a6c24136338886cc
jessicamvega6 <jessicamvega6@gmail.com>
2024-04-20 22:53:38 -0500

    Update Tools.java
    Added daily charge and other charge info to Tools enum so all info is in one spot

--- src/main/java/com/code/interviewDemo/domain/Tools.java
+++ src/main/java/com/code/interviewDemo/domain/Tools.java
@@ -1,19 +1,28 @@
 package com.code.interviewDemo.domain;
 
 public enum Tools {
-    CHAINSAW("CHNS", "Chainsaw", "Stihl"),
-    LADDER("LADW","Ladder","Werner"),
-    JACKHAMMER_D("JAKD","Jackhammer","DeWalt"),
-    JACKHAMMER_R("JAKR","Jackhammer","Ridgid");
+    CHAINSAW("CHNS", "Chainsaw", "Stihl", 1.49, true, false, true),
+    LADDER("LADW","Ladder","Werner", 1.99, true, true, false),
+    JACKHAMMER_D("JAKD","Jackhammer","DeWalt", 2.00, true, false, false),
+    JACKHAMMER_R("JAKR","Jackhammer","Ridgid", 2.00, true, false, false);
 
     final String toolCode;
     final String toolType;
     final String brand;
+    final double dailyCharge;
+    final boolean weekdayCharge;
+    final boolean weekendCharge;
+    final boolean holidayCharge;
 
-    Tools(String toolCode, String toolType, String brand) {
+    Tools(String toolCode, String toolType, String brand, double dailyCharge, boolean weekdayCharge,
+          boolean weekendCharge, boolean holidayCharge) {
         this.toolCode = toolCode;
         this.toolType = toolType;
         this.brand = brand;
+        this.dailyCharge = dailyCharge;
+        this.weekdayCharge = weekdayCharge;
+        this.weekendCharge = weekendCharge;
+        this.holidayCharge = holidayCharge;
     }
 
     public String getToolType() {
@@ -26,5 +35,21 @@ public enum Tools {
 
     public String getToolCode() {
         return toolCode;
+    }
+
+    public double getDailyCharge() {
+        return dailyCharge;
+    }
+
+    public boolean isWeekdayCharge() {
+        return weekdayCharge;
+    }
+
+    public boolean isWeekendCharge() {
+        return weekendCharge;
+    }
+
+    public boolean isHolidayCharge() {
+        return holidayCharge;
     }
 }
