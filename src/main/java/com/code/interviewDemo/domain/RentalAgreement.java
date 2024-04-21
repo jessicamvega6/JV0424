6fa62075ed8e521c086645a4c505dfdbf8e15c89
jessicamvega6 <jessicamvega6@gmail.com>
2024-04-20 22:46:33 -0500

    Update RentalAgreement.java
    Updated agreement to have getter/setter methods with dates and money variables formatted

--- src/main/java/com/code/interviewDemo/domain/RentalAgreement.java
+++ src/main/java/com/code/interviewDemo/domain/RentalAgreement.java
@@ -1,22 +1,128 @@
 package com.code.interviewDemo.domain;
 
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class RentalAgreement {
+    final DateTimeFormatter FORMMATER = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
     private String toolCode;
     private String toolType;
     private String toolBrand;
     private int rentalDays;
-    private Date checkoutDate;
-    private Date dueDate;
-    private Long dailyRentalCharge;
+    private LocalDate checkoutDate;
+    private LocalDate dueDate;
+    private Double dailyRentalCharge;
     private Integer chargeDays;
-    private Long preDiscountCharge;
-    private Long discountCharge;
+    private Double preDiscountCharge;
+    private Double discountCharge;
     private int discountPercent;
-    private Long discountAmount;
-    private Long finalCharge;
+    private Double discountAmount;
+    private Double finalCharge;
+
+    public String getToolCode() {
+        return toolCode;
+    }
+
+    public void setToolCode(String toolCode) {
+        this.toolCode = toolCode;
+    }
+
+    public String getToolType() {
+        return toolType;
+    }
+
+    public void setToolType(String toolType) {
+        this.toolType = toolType;
+    }
+
+    public String getToolBrand() {
+        return toolBrand;
+    }
+
+    public void setToolBrand(String toolBrand) {
+        this.toolBrand = toolBrand;
+    }
+
+    public int getRentalDays() {
+        return rentalDays;
+    }
+
+    public void setRentalDays(int rentalDays) {
+        this.rentalDays = rentalDays;
+    }
+
+    public Double getDailyRentalCharge() {
+        return Math.round(dailyRentalCharge * 100.0) / 100.0;
+    }
+
+    public void setDailyRentalCharge(Double dailyRentalCharge) {
+        this.dailyRentalCharge = dailyRentalCharge;
+    }
+
+    public Integer getChargeDays() {
+        return chargeDays;
+    }
+
+    public void setChargeDays(Integer chargeDays) {
+        this.chargeDays = chargeDays;
+    }
+
+    public Double getPreDiscountCharge() {
+        return Math.round(preDiscountCharge * 100.0) / 100.0;
+    }
+
+    public void setPreDiscountCharge(Double preDiscountCharge) {
+        this.preDiscountCharge = preDiscountCharge;
+    }
+
+    public Double getDiscountCharge() {
+        return Math.round(discountCharge * 100.0) / 100.0;
+    }
+
+    public void setDiscountCharge(Double discountCharge) {
+        this.discountCharge = discountCharge;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public Double getDiscountAmount() {
+        return Math.round(discountAmount * 100.0) / 100.0;
+    }
+
+    public void setDiscountAmount(Double discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public Double getFinalCharge() {
+        return Math.round(finalCharge * 100.0) / 100.0;
+    }
+
+    public void setFinalCharge(double finalCharge) {
+        this.finalCharge = finalCharge;
+    }
+
+    public String getDueDate() {
+        return dueDate.format(FORMMATER);
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getCheckoutDate() {
+        return checkoutDate.format(FORMMATER);
+    }
+
+    public void setCheckoutDate(LocalDate checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
 
     @Override
     public String toString() {
@@ -25,8 +131,8 @@ public class RentalAgreement {
                 "toolType='" + toolType + '\'' + '\n' +
                 "toolBrand='" + toolBrand + '\'' + '\n' +
                 "rentalDays=" + rentalDays + '\n' +
-                "checkoutDate=" + checkoutDate + '\n' +
-                "dueDate=" + dueDate + '\n' +
+                "checkoutDate=" + checkoutDate.format(FORMMATER) + '\n' +
+                "dueDate=" + dueDate.format(FORMMATER) + '\n' +
                 "dailyRentalCharge=" + dailyRentalCharge + '\n' +
                 "chargeDays=" + chargeDays + '\n' +
                 "preDiscountCharge=" + preDiscountCharge + '\n' +
